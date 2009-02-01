4fc0535da7470293552f2e805a381f57ccaebfa7
fadushin <fadushin@f3f41d29-57ee-40ef-bfb9-cef74e8ff843>
2009-02-01 01:19:24 +0000

    initial import
    git-svn-id: svn://quine.home/lethe/trunk@1 f3f41d29-57ee-40ef-bfb9-cef74e8ff843

---
+++ maven-projects/datamodel/src/test/java/net/dushin/lethe/datamodel/SchemaTest.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) dushin.net
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of dushin.net nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY dushin.net ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL dushin.net BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.dushin.lethe.datamodel;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ * Simple test to illustrate using junit in maven, jaxb, etc.
+ */
+public class SchemaTest {
+
+    /**
+     * @throws      Exception if an error occurred
+     */
+    @org.junit.Test
+    public final void
+    testLoadSchema() throws Exception {
+        try {
+            final JAXBContext ctx =
+                JAXBContext.newInstance(
+                    PublicKeyType.class.getPackage().getName().toString()
+                );
+            final Unmarshaller u = ctx.createUnmarshaller();
+            final java.net.URL url = getClass().getClassLoader().getResource(
+                "data/test.xml"
+            );
+            final Object o = u.unmarshal(url);
+            org.junit.Assert.assertTrue(
+                o instanceof PublicKeyType || o instanceof JAXBElement
+            );
+         } catch (final Exception e) {
+            e.printStackTrace();
+            org.junit.Assert.fail("unexpected exception");
+         }
+    }
+}
