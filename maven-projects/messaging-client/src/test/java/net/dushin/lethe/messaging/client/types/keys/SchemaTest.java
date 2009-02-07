5012ac88deabbf1eae986b23842b1eb31a1d12e0
fdushin <fdushin@f3f41d29-57ee-40ef-bfb9-cef74e8ff843>
2009-02-07 20:15:44 +0000

    cleanup and added RSA static content
    git-svn-id: svn://quine.home/lethe/trunk@13 f3f41d29-57ee-40ef-bfb9-cef74e8ff843

--- maven-projects/messaging-client/src/test/java/net/dushin/lethe/messaging/client/types/keys/SchemaTest.java
+++ maven-projects/messaging-client/src/test/java/net/dushin/lethe/messaging/client/types/keys/SchemaTest.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.dushin.lethe.messaging.rt.keys;
+package net.dushin.lethe.messaging.client.types.keys;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
