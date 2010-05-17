902392b40f5a28687c54444f8ddce98f61d7321b
(no author) <(no author)@f3f41d29-57ee-40ef-bfb9-cef74e8ff843>
2010-05-17 02:16:25 +0000

    Cleaned up the start script for testing outside of eclipse post now calls get_messages to update
    git-svn-id: svn://quine.home/lethe/trunk@128 f3f41d29-57ee-40ef-bfb9-cef74e8ff843

--- maven-projects/erlang/net_dushin_lethe/start.sh
+++ maven-projects/erlang/net_dushin_lethe/start.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-TARGET_DIR=/work/eclipse/workspace/3.4/lethe-messaging-server-erlang/erlang-server/ebin
+# TARGET_DIR=/work/eclipse/workspace/3.4/lethe-messaging-server-erlang/erlang-server/ebin
+TARGET_DIR=target/test
+BASE_DIR=$(pwd)
 
 cd ${TARGET_DIR}
 
+rm -rf content
 mkdir -p ./content
 mkdir -p ./logs
-echo "Hello World" > ./content/index.html
 
-exec erl -boot start_sasl -pa . -pa /opt/yaws/lib/yaws/ebin/ -yaws embedded true
+cd content
+ln -s /opt/yaws/var/yaws/www/jsolait
+ln -s ${BASE_DIR}/src/main/html/index.html
+cd ..
+
+exec erl -boot start_sasl -pa . -pa /opt/yaws/lib/yaws/ebin/ -s net_dushin_lethe_launcher start1 -yaws embedded true
 
 
 # application:start(net_dushin_lethe).
